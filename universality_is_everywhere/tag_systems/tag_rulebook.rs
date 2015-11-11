610a3ca3a5852509f554fd7b637d8330a9a85d1b
yodalee <lc85301@gmail.com>
2015-11-11 11:02:29 +0800

    impl tag system program

---
+++ universality_is_everywhere/tag_systems/tag_rulebook.rs
@@ -0,0 +1,38 @@
+use tag_rule::{TagRule};
+
+pub struct TagRulebook {
+    deletion_number: i32,
+    rules: Vec<TagRule>,
+}
+
+impl TagRulebook {
+    pub fn new(deletion_number: i32, rules: Vec<TagRule>) -> TagRulebook {
+        TagRulebook{
+            deletion_number: deletion_number,
+            rules: rules,
+        }
+    }
+
+    pub fn next_string(&self, input: &str) -> String {
+        match self.rule_for(input) {
+            Some(rule) => unsafe {
+                let newString = rule.follow(input);
+                newString.slice_unchecked(self.deletion_number as usize,
+                                                 newString.len()).to_string()
+            },
+            None => "".to_string(),
+        }
+    }
+
+    pub fn rule_for(&self, input: &str) -> Option<&TagRule> {
+        self.rules.iter().find(|rule| rule.applies_to(input))
+    }
+
+    pub fn applies_to(&self, input: &str) -> bool {
+        input.len() >= self.deletion_number as usize &&
+            match self.rule_for(input) {
+                Some(rule) => true,
+                None => false,
+        }
+    }
+}
