a319ce100e1037b6f3b99b4c48d1b921d271f19d
yodalee <lc85301@gmail.com>
2016-04-29 15:24:41 +0800

    impl lambda reduce

--- programming_with_nothing/lambda_calculus/lambda.rs
+++ programming_with_nothing/lambda_calculus/lambda.rs
@@ -26,10 +26,46 @@ impl Display for Lambda {
 }
 
 pub trait Reduce {
+    fn reducible(&self) -> bool;
+    fn reduce(&self) -> Box<Lambda>;
+    fn callable(&self) -> bool;
+    fn call(&self, &Box<Lambda>) -> Box<Lambda>;
     fn replace(&self, &str, &Box<Lambda>) -> Box<Lambda>;
 }
 
 impl Reduce for Lambda {
+    fn reducible(&self) -> bool {
+        match *self {
+            Lambda::LCFunction(_, _) | Lambda::LCVariable(_) => false,
+            Lambda::LCCall(ref l, ref r) => l.reducible() || r.reducible() || l.callable(),
+        }
+    }
+    fn reduce(&self) -> Box<Lambda> {
+        match *self {
+            Lambda::LCCall(ref l, ref r) => {
+                if l.reducible() {
+                    Lambda::lccall(l.reduce(), r.clone())
+                } else if r.reducible() {
+                    Lambda::lccall(l.clone(), r.reduce())
+                } else {
+                    l.call(r)
+                }
+            },
+            _ => panic!("Cannot reduce on {}", *self),
+        }
+    }
+    fn callable(&self) -> bool {
+        match *self {
+            Lambda::LCFunction(_, _) => true,
+            _ => false,
+        }
+    }
+    fn call(&self, argument: &Box<Lambda>) -> Box<Lambda> {
+        match *self {
+            Lambda::LCFunction(ref param, ref body) => body.replace(param, &argument),
+            _ => panic!("Cannot call on {}", *self),
+        }
+    }
     fn replace(&self, name: &str, replacement: &Box<Lambda>) -> Box<Lambda> {
         match *self {
             Lambda::LCVariable(ref varname) => {
--- programming_with_nothing/lambda_calculus/main.rs
+++ programming_with_nothing/lambda_calculus/main.rs
@@ -36,4 +36,37 @@ pub fn main() {
             Lambda::lcvar("b"));
     println!("{}", expr.replace("a", &Lambda::lcvar("x")));
     println!("{}", expr.replace("b", &Lambda::lcfun("x", Lambda::lcvar("x"))));
+
+    expr = Lambda::lcfun("y", Lambda::lccall(Lambda::lcvar("x"), Lambda::lcvar("y")));
+    println!("{}", expr.replace("x", &Lambda::lcvar("z")));
+    println!("{}", expr.replace("y", &Lambda::lcvar("z")));
+
+    expr = Lambda::lccall(
+        Lambda::lccall(Lambda::lcvar("x"), Lambda::lcvar("y")),
+        Lambda::lcfun("y", Lambda::lccall(Lambda::lcvar("y"), Lambda::lcvar("x")))
+    );
+
+    println!("{}", expr.replace("x", &Lambda::lcvar("z")));
+    println!("{}", expr.replace("y", &Lambda::lcvar("z")));
+
+    let func = Lambda::lcfun("x", Lambda::lcfun("y", Lambda::lccall(Lambda::lcvar("x"), Lambda::lcvar("y"))));
+    let arg = Lambda::lcfun("z", Lambda::lcvar("z"));
+    println!("{}", func.call(&arg));
+
+    expr = Lambda::lccall(Lambda::lccall(add, one.clone()), one.clone());
+    while expr.reducible() {
+        println!("{}", expr);
+        expr = expr.reduce()
+    }
+    println!("{}", expr);
+
+    let inc = Lambda::lcvar("inc");
+    let zero = Lambda::lcvar("zero");
+
+    expr = Lambda::lccall(Lambda::lccall(expr, inc), zero);
+    while expr.reducible() {
+        println!("{}", expr);
+        expr = expr.reduce()
+    }
+    println!("{}", expr);
 }
