791b576fa12e3f2f300953f694ef633dbe7974c6
yodalee <lc85301@gmail.com>
2018-04-05 01:51:37 +0800

    move main function to test module

--- src/just_add_power/main.rs
+++ src/just_add_power/main.rs
@@ -18,121 +18,221 @@ use npda::{NPDA};
 use npdadesign::{NPDADesign};
 use helper::{to_hashset};
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dpa_config() {
+        let rule = PDARule::new(1, '(', 2, '$', &['b', '$']);
+        println!("{}", rule);
+        let config = PDAConfiguration::new(1, &['$']);
+        println!("{}", config);
+        assert!(rule.applies_to(&config, '('));
+        println!("{}", rule.follow(&config));
+    }
+
+    #[test]
+    fn test_dpa_rulebook() {
+        let mut config = PDAConfiguration::new(1, &['$']);
+        let rulebook = DPDARulebook::new(
+            &[PDARule::new(1, '(', 2, '$', &['b', '$']),
+              PDARule::new(2, '(', 2, 'b', &['b', 'b']),
+              PDARule::new(2, ')', 2, 'b', &[]),
+              PDARule::new(2, '\0', 1, '$', &['$'])]);
+        config = rulebook.next_config(&config, '(');
+        println!("{}", config);
+        assert_eq!(config.stack, &['$', 'b']);
+        config = rulebook.next_config(&config, '(');
+        println!("{}", config);
+        assert_eq!(config.stack, &['$', 'b', 'b']);
+        config = rulebook.next_config(&config, ')');
+        println!("{}", config);
+        assert_eq!(config.stack, &['$', 'b']);
+    }
+
+    #[test]
+    fn test_dpda_string() {
+        let rulebook = DPDARulebook::new(
+            &[PDARule::new(1, '(', 2, '$', &['b', '$']),
+              PDARule::new(2, '(', 2, 'b', &['b', 'b']),
+              PDARule::new(2, ')', 2, 'b', &[]),
+              PDARule::new(2, '\0', 1, '$', &['$'])]);
+        let mut dpda = DPDA::new(&PDAConfiguration::new(1, &['$']), &[1], &rulebook);
+        assert!(dpda.accept());
+        dpda.read_string("(()");
+        assert!(!dpda.accept());
+        assert_eq!(dpda.config.stack, &['$', 'b']);
+    }
+
+    #[test]
+    fn test_freemove() {
+        let mut config = PDAConfiguration::new(2, &['$']);
+        let rulebook = DPDARulebook::new(
+            &[PDARule::new(1, '(', 2, '$', &['b', '$']),
+              PDARule::new(2, '(', 2, 'b', &['b', 'b']),
+              PDARule::new(2, ')', 2, 'b', &[]),
+              PDARule::new(2, '\0', 1, '$', &['$'])]);
+        assert_eq!(config.state, Some(2));
+        config = rulebook.follow_free_moves(&config);
+        assert_eq!(config.state, Some(1));
+
+        /* blow up
+        DPDARulebook::new(&[PDARule::new(1, '\0', 1, '$', &['$'])])
+            .follow_free_moves(&PDAConfiguration::new(1, &['$']));
+        */
+    }
+
+    #[test]
+    fn test_dpda() {
+        let rulebook = DPDARulebook::new(
+            &[PDARule::new(1, '(', 2, '$', &['b', '$']),
+              PDARule::new(2, '(', 2, 'b', &['b', 'b']),
+              PDARule::new(2, ')', 2, 'b', &[]),
+              PDARule::new(2, '\0', 1, '$', &['$'])]);
+        let mut dpda = DPDA::new(&PDAConfiguration::new(1, &['$']), &[1], &rulebook);
+        dpda.read_string("(()(");
+        assert!(!dpda.accept());
+        assert_eq!(dpda.current_config().state, Some(2));
+        assert_eq!(dpda.current_config().stack, &['$', 'b', 'b']);
+        dpda.read_string("))()");
+        assert!(dpda.accept());
+        assert_eq!(dpda.current_config().state, Some(1));
+        assert_eq!(dpda.current_config().stack, &['$']);
+    }
+
+    #[test]
+    fn test_dpda_design() {
+        let rulebook = DPDARulebook::new(
+            &[PDARule::new(1, '(', 2, '$', &['b', '$']),
+              PDARule::new(2, '(', 2, 'b', &['b', 'b']),
+              PDARule::new(2, ')', 2, 'b', &[]),
+              PDARule::new(2, '\0', 1, '$', &['$'])]);
+        let dpda_design = DPDADesign::new(1, '$', &[1], &rulebook);
+        assert!(dpda_design.accept("(((((((((())))))))))"));
+        assert!(dpda_design.accept("()(())((()))(()(()))"));
+        assert!(!dpda_design.accept("(()(()(()()(()()))()"));
+        assert!(!dpda_design.accept("())"));
+    }
+
+    #[test]
+    fn test_dpda_stuck() {
+        let rulebook = DPDARulebook::new(
+            &[PDARule::new(1, '(', 2, '$', &['b', '$']),
+              PDARule::new(2, '(', 2, 'b', &['b', 'b']),
+              PDARule::new(2, ')', 2, 'b', &[]),
+              PDARule::new(2, '\0', 1, '$', &['$'])]);
+        let mut dpda = DPDA::new(&PDAConfiguration::new(1, &['$']), &[1], &rulebook);
+        dpda.read_string("())");
+        assert_eq!(dpda.config.state, None);
+        assert!(!dpda.accept());
+        assert!(dpda.is_stuck());
+
+        let dpda_design = DPDADesign::new(1, '$', &[1], &rulebook);
+        assert!(!dpda_design.accept("())"));
+    }
+
+    #[test]
+    fn test_dpda_stringab() {
+        let rulebook = DPDARulebook::new(&[
+            PDARule::new(1, 'a', 2, '$', &['a', '$']),
+            PDARule::new(1, 'b', 2, '$', &['b', '$']),
+            PDARule::new(2, 'a', 2, 'a', &['a', 'a']),
+            PDARule::new(2, 'b', 2, 'b', &['b', 'b']),
+            PDARule::new(2, 'a', 2, 'b', &[]),
+            PDARule::new(2, 'b', 2, 'a', &[]),
+            PDARule::new(2, '\0', 1, '$', &['$'])
+        ]);
+        let dpda_design = DPDADesign::new(1, '$', &[1], &rulebook);
+        assert!(dpda_design.accept("ababab"));
+        assert!(dpda_design.accept("bbbaaaab"));
+        assert!(!dpda_design.accept("baa"));
+    }
+
+    #[test]
+    fn test_dpda_palindrome_stringab() {
+        let rulebook = DPDARulebook::new(&[
+            PDARule::new(1, 'a', 1, '$', &['a', '$']),
+            PDARule::new(1, 'a', 1, 'a', &['a', 'a']),
+            PDARule::new(1, 'a', 1, 'b', &['a', 'b']),
+            PDARule::new(1, 'b', 1, '$', &['b', '$']),
+            PDARule::new(1, 'b', 1, 'a', &['b', 'a']),
+            PDARule::new(1, 'b', 1, 'b', &['b', 'b']),
+            PDARule::new(1, 'm', 2, '$', &['$']),
+            PDARule::new(1, 'm', 2, 'a', &['a']),
+            PDARule::new(1, 'm', 2, 'b', &['b']),
+            PDARule::new(2, 'a', 2, 'a', &[]),
+            PDARule::new(2, 'b', 2, 'b', &[]),
+            PDARule::new(2, '\0', 3, '$', &['$'])
+        ]);
+        let dpda_design = DPDADesign::new(1, '$', &[3], &rulebook);
+        println!("demo: palindrome with m in middle");
+        assert!(dpda_design.accept("abmba"));
+        assert!(dpda_design.accept("babbamabbab"));
+        assert!(!dpda_design.accept("abmb"));
+        assert!(!dpda_design.accept("baambaa"));
+    }
+
+    #[test]
+    fn test_npda() {
+        let rulebook = NPDARulebook::new(&[
+            PDARule::new(1, 'a', 1, '$', &['a', '$']),
+            PDARule::new(1, 'a', 1, 'a', &['a', 'a']),
+            PDARule::new(1, 'a', 1, 'b', &['a', 'b']),
+            PDARule::new(1, 'b', 1, '$', &['b', '$']),
+            PDARule::new(1, 'b', 1, 'a', &['b', 'a']),
+            PDARule::new(1, 'b', 1, 'b', &['b', 'b']),
+            PDARule::new(1, '\0', 2, '$', &['$']),
+            PDARule::new(1, '\0', 2, 'a', &['a']),
+            PDARule::new(1, '\0', 2, 'b', &['b']),
+            PDARule::new(2, 'a', 2, 'a', &[]),
+            PDARule::new(2, 'b', 2, 'b', &[]),
+            PDARule::new(2, '\0', 3, '$', &['$'])
+        ]);
+        let config = PDAConfiguration::new(1, &['$']);
+        let mut npda = NPDA::new(&to_hashset(&[config]), &to_hashset(&[3]), &rulebook);
+
+        assert!(npda.accept());
+        for config in npda.current_config().iter() {
+            println!("{}", config);
+        }
+
+        npda.read_string("abb");
+        assert!(!npda.accept());
+        for config in npda.current_config().iter() {
+            println!("{}", config);
+        }
+
+        npda.read_character('a');
+        assert!(npda.accept());
+        for config in npda.current_config().iter() {
+            println!("{}", config);
+        }
+    }
+
+    #[test]
+    fn test_npda_palindrome() {
+        let rulebook = NPDARulebook::new(&[
+            PDARule::new(1, 'a', 1, '$', &['a', '$']),
+            PDARule::new(1, 'a', 1, 'a', &['a', 'a']),
+            PDARule::new(1, 'a', 1, 'b', &['a', 'b']),
+            PDARule::new(1, 'b', 1, '$', &['b', '$']),
+            PDARule::new(1, 'b', 1, 'a', &['b', 'a']),
+            PDARule::new(1, 'b', 1, 'b', &['b', 'b']),
+            PDARule::new(1, '\0', 2, '$', &['$']),
+            PDARule::new(1, '\0', 2, 'a', &['a']),
+            PDARule::new(1, '\0', 2, 'b', &['b']),
+            PDARule::new(2, 'a', 2, 'a', &[]),
+            PDARule::new(2, 'b', 2, 'b', &[]),
+            PDARule::new(2, '\0', 3, '$', &['$'])
+        ]);
+        let npda_design = NPDADesign::new(1, '$', &[3], &rulebook);
+        assert!(npda_design.accept("abba"));
+        assert!(npda_design.accept("babbaabbab"));
+        assert!(!npda_design.accept("abb"));
+        assert!(!npda_design.accept("baabaa"));
+    }
+}
+
 pub fn main() {
-    println!("************************************");
-    println!("Demo Deterministic PushDown Automata");
-    println!("************************************");
-    let rule = PDARule::new(1, '(', 2, '$', &['b', '$']);
-    println!("{}", rule);
-    let mut config = PDAConfiguration::new(1, &['$']);
-    println!("{}", config);
-    println!("rule applies to? {}", rule.applies_to(&config, '('));
-    println!("{}", rule.follow(&config));
-
-    let mut rulebook = DPDARulebook::new(
-        &[PDARule::new(1, '(', 2, '$', &['b', '$']), PDARule::new(2, '(', 2, 'b', &['b', 'b']),
-        PDARule::new(2, ')', 2, 'b', &[]), PDARule::new(2, '\0', 1, '$', &['$'])]);
-    config = rulebook.next_config(&config, '(');
-    println!("{}", config);
-    config = rulebook.next_config(&config, '(');
-    println!("{}", config);
-    config = rulebook.next_config(&config, ')');
-    println!("{}", config);
-
-    let mut dpda = DPDA::new(&PDAConfiguration::new(1, &['$']), &[1], &rulebook);
-    println!("accept '' {}", dpda.accept());
-    dpda.read_string("(()");
-    println!("accept '(()' {}", dpda.accept());
-    println!("config {}", dpda.config);
-
-    config = PDAConfiguration::new(2, &['$']);
-    println!("{}", config);
-    println!("{}", rulebook.follow_free_moves(&config));
-
-    //blow up
-    //DPDARulebook::new(&[PDARule::new(1, '\0', 1, '$', &['$'])])
-    //    .follow_free_moves(&PDAConfiguration::new(1, &['$']));
-
-    dpda = DPDA::new(&PDAConfiguration::new(1, &['$']), &[1], &rulebook);
-    dpda.read_string("(()(");
-    println!("accept? {}", dpda.accept());
-    println!("config: {}", dpda.current_config());
-    dpda.read_string("))()");
-    println!("accept? {}", dpda.accept());
-    println!("config: {}", dpda.current_config());
-
-    let mut dpda_design = DPDADesign::new(1, '$', &[1], &rulebook);
-    println!("accept: '(((((((((())))))))))': {}", dpda_design.accept("(((((((((())))))))))"));
-    println!("accept: '()(())((()))(()(()))': {}", dpda_design.accept("()(())((()))(()(()))"));
-    println!("accept: '(()(()(()()(()()))()': {}", dpda_design.accept("(()(()(()()(()()))()"));
-
-    //blow up println!("accept: '())': {}", dpda_design.accept("())"));
-
-    dpda = DPDA::new(&PDAConfiguration::new(1, &['$']), &[1], &rulebook);
-    dpda.read_string("())");
-    println!("{}", dpda.config);
-    println!("accept? {}", dpda.accept());
-    println!("stuck? {}", dpda.is_stuck());
-    println!("{}", dpda_design.accept("())"));
-
-    rulebook = DPDARulebook::new(&[PDARule::new(1, 'a', 2, '$', &['a', '$']),
-        PDARule::new(1, 'b', 2, '$', &['b', '$']), PDARule::new(2, 'a', 2, 'a', &['a', 'a']),
-        PDARule::new(2, 'b', 2, 'b', &['b', 'b']), PDARule::new(2, 'a', 2, 'b', &[]),
-        PDARule::new(2, 'b', 2, 'a', &[]), PDARule::new(2, '\0', 1, '$', &['$'])
-    ]);
-    dpda_design = DPDADesign::new(1, '$', &[1], &rulebook);
-    println!("demo: contains equal a, b: accept ababab? {}, bbbaaaab? {}, baa? {}",
-        dpda_design.accept("ababab"), dpda_design.accept("bbbaaaab"), dpda_design.accept("baa"));
-
-    rulebook = DPDARulebook::new(&[PDARule::new(1, 'a', 1, '$', &['a', '$']),
-        PDARule::new(1, 'a', 1, 'a', &['a', 'a']), PDARule::new(1, 'a', 1, 'b', &['a', 'b']),
-        PDARule::new(1, 'b', 1, '$', &['b', '$']), PDARule::new(1, 'b', 1, 'a', &['b', 'a']),
-        PDARule::new(1, 'b', 1, 'b', &['b', 'b']), PDARule::new(1, 'm', 2, '$', &['$']),
-        PDARule::new(1, 'm', 2, 'a', &['a']), PDARule::new(1, 'm', 2, 'b', &['b']),
-        PDARule::new(2, 'a', 2, 'a', &[]), PDARule::new(2, 'b', 2, 'b', &[]),
-        PDARule::new(2, '\0', 3, '$', &['$'])
-    ]);
-    dpda_design = DPDADesign::new(1, '$', &[3], &rulebook);
-    println!("demo: palindrome with m in middle");
-    println!("accept abmba?: {}, accept babbamabbab?: {}, accept abmb?: {}, accept baambaa?: {}",
-        dpda_design.accept("abmba"), dpda_design.accept("babbamabbab"), dpda_design.accept("abmb"), dpda_design.accept("baambaa"));
-
-    println!("****************************************");
-    println!("Demo Non-Deterministic PushDown Automata");
-    println!("****************************************");
-
-    let rulebook = NPDARulebook::new(&[
-        PDARule::new(1, 'a', 1, '$', &['a', '$']),
-        PDARule::new(1, 'a', 1, 'a', &['a', 'a']),
-        PDARule::new(1, 'a', 1, 'b', &['a', 'b']),
-        PDARule::new(1, 'b', 1, '$', &['b', '$']),
-        PDARule::new(1, 'b', 1, 'a', &['b', 'a']),
-        PDARule::new(1, 'b', 1, 'b', &['b', 'b']),
-        PDARule::new(1, '\0', 2, '$', &['$']),
-        PDARule::new(1, '\0', 2, 'a', &['a']),
-        PDARule::new(1, '\0', 2, 'b', &['b']),
-        PDARule::new(2, 'a', 2, 'a', &[]),
-        PDARule::new(2, 'b', 2, 'b', &[]),
-        PDARule::new(2, '\0', 3, '$', &['$'])
-    ]);
-    config = PDAConfiguration::new(1, &['$']);
-    let mut npda = NPDA::new(&to_hashset(&[config]), &to_hashset(&[3]), &rulebook);
-    println!("{}", npda.accept());
-    for config in npda.current_config().iter() {
-        println!("{}", config);
-    }
-    npda.read_string("abb");
-    println!("{}", npda.accept());
-    for config in npda.current_config().iter() {
-        println!("{}", config);
-    }
-    npda.read_character('a');
-    println!("{}", npda.accept());
-    for config in npda.current_config().iter() {
-        println!("{}", config);
-    }
-
-    let npda_design = NPDADesign::new(1, '$', &[3], &rulebook);
-    println!("accept abba? {}, babbaabbab? {}, abb? {}, baabaa? {}",
-        npda_design.accept("abba"), npda_design.accept("babbaabbab"), npda_design.accept("abb"), npda_design.accept("baabaa"));
 }
