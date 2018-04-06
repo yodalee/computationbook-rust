8ec85fe258c433198ebaba3604760c54d955b693
yodalee <lc85301@gmail.com>
2018-04-06 17:53:13 +0800

    migrate cyclic_tag_system to test

--- src/universality_is_everywhere/cyclic_tag_systems/mod.rs
+++ src/universality_is_everywhere/cyclic_tag_systems/mod.rs
@@ -3,47 +3,70 @@ mod cyclic_tag_rulebook;
 mod cyclic_tag_system;
 mod tag_to_cyclic;
 
-use universality_is_everywhere::tag_systems::tag_rule::{TagRule};
-use universality_is_everywhere::tag_systems::tag_rulebook::{TagRulebook};
-use universality_is_everywhere::tag_systems::tag_system::{TagSystem};
+#[cfg(test)]
+mod tests {
+    use universality_is_everywhere::tag_systems::tag_rule::{TagRule};
+    use universality_is_everywhere::tag_systems::tag_rulebook::{TagRulebook};
+    use universality_is_everywhere::tag_systems::tag_system::{TagSystem};
 
-use self::cyclic_tag_rule::{CyclicTagRule};
-use self::cyclic_tag_rulebook::{CyclicTagRulebook};
-use self::cyclic_tag_system::{CyclicTagSystem};
-use self::tag_to_cyclic::{TagToCyclic};
+    use super::cyclic_tag_rule::{CyclicTagRule};
+    use super::cyclic_tag_rulebook::{CyclicTagRulebook};
+    use super::cyclic_tag_system::{CyclicTagSystem};
+    use super::tag_to_cyclic::{TagToCyclic};
 
-pub fn main() {
-    let rulebook = CyclicTagRulebook::new(&[CyclicTagRule::new("1"), CyclicTagRule::new("0010"), CyclicTagRule::new("10")].to_vec());
-
-    let mut system = CyclicTagSystem::new("11", rulebook);
-
-    for _ in 0..16 {
-        println!("{}", system.current_string);
-        system.step();
+    fn print_assert(expr: &str, name: &str, ans: &str) {
+        let fmtstr = format!("{}", expr);
+        println!("{}: {}", name, fmtstr);
+        assert_eq!(ans, fmtstr);
     }
-    println!("{}", system.current_string);
 
-    for _ in 0..20 {
-        println!("{}", system.current_string);
-        system.step();
+    #[test]
+    fn test_cyclic_demo() {
+        let rulebook = CyclicTagRulebook::new(&[CyclicTagRule::new("1"), CyclicTagRule::new("0010"), CyclicTagRule::new("10")].to_vec());
+        let mut system = CyclicTagSystem::new("11", rulebook);
+        for _ in 0..16 {
+            println!("{}", system.current_string);
+            system.step();
+        }
+        print_assert(&system.current_string, "period 16 times", "00101");
+        for _ in 0..20 {
+            println!("{}", system.current_string);
+            system.step();
+        }
+        print_assert(&system.current_string, "period +20 times", "101");
     }
-    println!("{}", system.current_string);
 
-    let tagrulebook = TagRulebook::new(2, [TagRule::new('a', "ccdd"), TagRule::new('b', "dd")].to_vec());
-    let tagsystem = TagSystem::new("aabbbb", tagrulebook);
-    println!("{:?}", tagsystem.alphabet());
+    #[test]
+    fn test_cyclic_alphabet() {
+        let tagrulebook = TagRulebook::new(2, [TagRule::new('a', "ccdd"), TagRule::new('b', "dd")].to_vec());
+        let tagsystem = TagSystem::new("aabbbb", tagrulebook);
+        assert_eq!(tagsystem.alphabet(), &['a', 'b', 'c', 'd']);
+    }
 
-    let encoder = tagsystem.encoder();
-    println!("{}", encoder.encode_character('c'));
-    println!("{}", encoder.encode_string("cab"));
+    #[test]
+    fn test_cyclic_encoder() {
+        let tagrulebook = TagRulebook::new(2, [TagRule::new('a', "ccdd"), TagRule::new('b', "dd")].to_vec());
+        let tagsystem = TagSystem::new("aabbbb", tagrulebook);
+        let encoder = tagsystem.encoder();
+        assert_eq!("0010", encoder.encode_character('c'));
+        assert_eq!("001010000100", encoder.encode_string("cab"));
+    }
 
-    let rule = tagsystem.rulebook().rules()[0].clone();
-    println!("{:?}", rule.to_cyclic(&encoder));
+    #[test]
+    fn test_cyclic_tocyclic() {
+        let tagrulebook = TagRulebook::new(2, [TagRule::new('a', "ccdd"), TagRule::new('b', "dd")].to_vec());
+        let tagsystem = TagSystem::new("aabbbb", tagrulebook);
+        let encoder = tagsystem.encoder();
+        let rule = tagsystem.rulebook().rules()[0].clone();
+        let cycrule = rule.to_cyclic(&encoder);
+        println!("{}", cycrule); //rule.to_cyclic(&encoder));
+        assert_eq!(format!("{}", cycrule), "<CyclicTagRule 0010001000010001>");
 
-    println!("{:?}", tagsystem.rulebook().cyclic_rules(&encoder));
-    println!("{:?}", tagsystem.rulebook().cyclic_padding_rules(&encoder));
+        println!("{:?}", tagsystem.rulebook().cyclic_rules(&encoder));
+        println!("{:?}", tagsystem.rulebook().cyclic_padding_rules(&encoder));
 
-    let mut cyclic_system = tagsystem.to_cyclic(&encoder);
+        let mut cyclic_system = tagsystem.to_cyclic(&encoder);
 
-    cyclic_system.run()
+        cyclic_system.run();
+    }
 }
